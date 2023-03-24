73d75b73dee866e1240f5f1978778ebd4171a4a3
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-24 15:26:40 +0100

    make generetor structor post

--- asset/js/main.js
+++ asset/js/main.js
@@ -55,3 +55,126 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+let media = 5555555
+let name = 666666
+let time = 777777777
+let text = 888888888
+let image = 999999999
+let positionPost = 4444444444
+let numberOfLike = 3333333
+const containerEl = document.getElementById('container')
+const postGen = generated_post(media,name,time,text,image,positionPost,numberOfLike)
+containerEl.insertAdjacentElement('beforeend', postGen)
+
+function generated_post(media,name,time,text,image,positionPost,numberOfLike) {
+
+    const post = document.createElement('div')
+    post.classList.add('post')
+    post.insertAdjacentElement ('beforeend',generated_post_header(media,name,time))
+    post.insertAdjacentElement ('beforeend',generated_post_text(text))
+    post.insertAdjacentElement ('beforeend',generated_post_image(image))
+    post.insertAdjacentElement ('beforeend',generated_post_footer(positionPost,numberOfLike))
+
+    
+
+    return post
+}
+//------------------------POST HEADER----------------------------------------------------------
+
+function generated_post_header(media,name,time) {
+    const post_header = document.createElement('div')
+    post_header.classList.add('post__header')
+    post_header.insertAdjacentElement('beforeend',generated_post_meta(media,name,time))
+    return post_header
+}
+function generated_post_meta(media,name,time) {
+    const post_meta = document.createElement('div')
+    post_meta.classList.add('post-meta')
+    post_meta.insertAdjacentElement('beforeend',generated_post_meta_icon(media))
+    post_meta.insertAdjacentElement('beforeend',generated_post_meta_data(name,time))
+    return post_meta
+}
+function generated_post_meta_icon(media) {
+    const post_meta_icon = document.createElement('div')
+    post_meta_icon.classList.add('post-meta__icon')
+    const icon = document.createElement('img')
+    icon.classList.add('profile-pic')
+    icon.src = `${media}`
+    post_meta_icon.insertAdjacentElement('beforeend',icon)
+    return post_meta_icon
+}
+function generated_post_meta_data(name,time) {
+    const post_meta_data = document.createElement('div')
+    post_meta_data.classList.add('post-meta__data')
+    post_meta_data.insertAdjacentElement('beforeend',generated_post_meta_data_author(name))
+    post_meta_data.insertAdjacentElement('beforeend', generated_post_meta_data_author(time))
+    return post_meta_data
+}
+function generated_post_meta_data_author(name) {
+    const post_meta_data_author = document.createElement('div')
+    post_meta_data_author.classList.add('post-meta__author')
+    post_meta_data_author.innerText = `${name}`
+    return post_meta_data_author
+}
+function generated_post_meta_data_author(time) {
+    const post_meta_data_author = document.createElement('div')
+    post_meta_data_author.classList.add('post-meta__time')
+    post_meta_data_author.innerText = `${time}`
+    return post_meta_data_author
+}
+
+//---------------------------POST TEXT---------------------------------------
+
+function generated_post_text(text) {
+    const post_text = document.createElement('div')
+    post_text.classList.add('post__text')
+    post_text.innerText = `${text}`
+    return post_text
+}
+
+//-----------------------------POST IMAGE------------------------------------
+function generated_post_image(image) {
+    const post_image = document.createElement('div')
+    post_image.classList.add('post__image')
+    const img = document.createElement('img')
+    img.src = `${image}`
+    post_image.insertAdjacentElement('beforeend',img)
+    
+    return post_image
+}
+//---------------------------POST FOOTER------------------------------------------
+function generated_post_footer(positionPost,numberOfLike) {
+    const post_footer = document.createElement('div')
+    post_footer.classList.add('post__footer')
+    post_footer.insertAdjacentElement('beforeend',generated_likes(positionPost,numberOfLike))
+    return post_footer
+}
+
+function generated_likes(positionPost,numberOfLike) {
+    const post_footer_likes = document.createElement('div')
+    post_footer_likes.classList.add('likes','js-likes')
+    post_footer_likes.insertAdjacentElement('beforeend',generated_likes_cta(positionPost))
+    post_footer_likes.insertAdjacentElement('beforeend',nagenerated_likes_counter(numberOfLike))
+    return post_footer_likes
+}
+
+function generated_likes_cta(positionPost) {
+    const post_footer_likes_cta = document.createElement('div')
+    post_footer_likes_cta.classList.add('likes__cta')
+    const aEl = `
+    <a class="like-button  js-like-button" href="#" data-postid="${positionPost}">
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+    </a>
+    `
+    
+    post_footer_likes_cta.insertAdjacentHTML('beforeend',aEl)
+    return post_footer_likes_cta
+}
+
+function nagenerated_likes_counter(numberOfLike) {
+    const post_footer_likes_counter = document.createElement('div')
+    post_footer_likes_counter.classList.add('likes__counter')
+    post_footer_likes_counter.innerHTML = `Piace a <b id="like-counter-1" class="js-likes-counter">${numberOfLike}</b> persone`
+    return post_footer_likes_counter
+}
